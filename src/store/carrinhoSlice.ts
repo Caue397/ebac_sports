faf59cb631ab086a78d4ec2b9f0c6a47d52eccfa
Cauê <cauegallizzi397@gmail.com>
2026-02-28 17:32:01 -0300

    task (Global State): Use redux to provide a global state

---
+++ src/store/carrinhoSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../types'
+
+type CarrinhoState = {
+  itens: Produto[]
+  favoritos: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  itens: [],
+  favoritos: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionarAoCarrinho(state, action: PayloadAction<Produto>) {
+      const jaExiste = state.itens.find((p) => p.id === action.payload.id)
+      if (!jaExiste) {
+        state.itens.push(action.payload)
+      }
+    },
+    favoritar(state, action: PayloadAction<Produto>) {
+      const jaExiste = state.favoritos.find((p) => p.id === action.payload.id)
+      if (jaExiste) {
+        state.favoritos = state.favoritos.filter(
+          (p) => p.id !== action.payload.id
+        )
+      } else {
+        state.favoritos.push(action.payload)
+      }
+    }
+  }
+})
+
+export const { adicionarAoCarrinho, favoritar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
